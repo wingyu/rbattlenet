942d15c310d47de994fb147be950a63434fc3b7c
Vincent Wong <wingyu64@gmail.com>
2015-09-27 14:53:46 +1000

    fixed up specs for query fields

--- spec/lib/query_fields_spec.rb
+++ spec/lib/query_fields_spec.rb
@@ -10,9 +10,12 @@ describe "non-US region and locale functionality" do
     it "fetches parses and fetches optional field data" do
       VCR.use_cassette('query_fields') do
         character = RBattlenet::Wow::Character.
-          find("milhause", "saurfang", "pet slots")
+          find("milhause", "saurfang", ["pet slots", "guild"])
 
-        expect(character['petSlots'].count).to eq 3
+        aggregate_failures do
+          expect(character['petSlots'].count).to eq 3
+          expect(character['guild']['name']).to eq "Razors Edge"
+        end
       end
     end
   end
