32bdb83963a3b39291902e7b523d4604c47fe195
Vincent <vincent@Vincents-MacBook-Pro.local>
2015-09-28 19:34:40 +1000

    S2::Profile

---
+++ lib/rbattlenet/s2/data_resources.rb
@@ -0,0 +1,23 @@
+module RBattlenet
+  module S2
+    class DataResources
+      def self.find_item(data)
+        uri = RBattlenet.base_uri("#{GAME}/data/item/#{data}")
+
+        RBattlenet.get(uri)
+      end
+
+      def self.find_follower(follower)
+        uri = RBattlenet.base_uri("#{GAME}/data/follower/#{follower}")
+
+        RBattlenet.get(uri)
+      end
+
+      def self.find_artisan(artisan)
+        uri = RBattlenet.base_uri("#{GAME}/data/artisan/#{artisan}")
+
+        RBattlenet.get(uri)
+      end
+    end
+  end
+end
---
+++ lib/rbattlenet/s2/profile.rb
@@ -0,0 +1,17 @@
+module RBattlenet
+  module S2
+    class Profile
+      def self.find_career(battletag)
+        uri = RBattlenet.base_uri("#{GAME}/profile/#{battletag}/")
+
+        RBattlenet.get(uri)
+      end
+
+      def self.find_hero(battletag, id)
+        uri = RBattlenet.base_uri("#{GAME}/profile/#{battletag}/hero/#{id}")
+
+        RBattlenet.get(uri)
+      end
+    end
+  end
+end
