cfb8479bc2d4cd750d1d6a9ebd3907e0d1db70d0
Etienne Ischer <etienne.ischer@gmail.com>
2020-10-13 22:55:56 +0200

    fix leading slash errors

--- lib/rbattlenet/endpoints/wow/profile/pets_collection.rb
+++ lib/rbattlenet/endpoints/wow/profile/pets_collection.rb
@@ -2,7 +2,7 @@ module RBattlenet
   module Wow
     class Profile::PetsCollection < RBattlenet::Endpoints::Base
       def self.path(token)
-        RBattlenet.uri("/profile/user/wow/collections/pets?access_token=#{token}&namespace=profile-")
+        RBattlenet.uri("profile/user/wow/collections/pets?access_token=#{token}&namespace=profile-")
       end
     end
   end
